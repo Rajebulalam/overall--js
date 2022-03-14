f2ef21324554c005e3d4c46db8fd9f07aed2773f
unknown <rajebul.alam17@gmail.com>
2022-03-14 14:48:19 +0600

    Variable Details Commited

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1. Six Basics Fundamentals
+
+/* 
+    1.1 variable (const, let)
+        const => const variable are declare then, when we know that the variable value couldn't be change.
+        Ex. 
+            const name = 'Rajebul Alam Khokan';
+            console.log(name);
+
+        let => let variable are declare then, when we know that the varible value could be change or assign a new value.
+        Ex.
+            let salary = 50000;
+            salary = 100000;
+            console.log(salary);
+
+        => Three Types of Variable
+        1. string = string means assigning text.
+            Ex.
+                const string = 'Rajebul Alam Khokan';
+                console.log(typeof string); 
+
+        2. number or numeric = number means means assigning number.
+            Ex.
+                const number = 12345;
+                console.log(typeof number);
+
+        3. boolean = boolean means a condition that could be true or false.
+            Ex.
+                const isActive = true;
+                console.log(typeof isActive);
+*/
+
