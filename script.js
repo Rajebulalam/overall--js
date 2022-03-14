0782616e6ee7550dab6669e4675bf6347666ed1a
unknown <rajebul.alam17@gmail.com>
2022-03-14 15:11:46 +0600

    Six Basics Condition

--- script.js
+++ script.js
@@ -30,3 +30,63 @@
                 console.log(typeof isActive);
 */
 
+/* 
+    1.2 Condition
+        Condition means some rules that's are compare one with another and also with some restriction like if you will follow rules then ok otherwise not.
+
+        * Six Basics Condition
+            1. > (Greater Then) That means the left side element will be bigger then the right side.
+            Ex.
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary > money);
+
+                *Sigle Line 
+                console.log(5 > 4);
+
+            2. < (Less Then) That means the left side element will be smaller then the right side.
+            Ex. 
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary < money);
+
+                *Single Line
+                console.log(5 < 6);
+
+            3. === (Equal in Every Angle) That means the left side and the right side element will be equal in every angle like typeOf, number, chracter etc.
+            Ex. 
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary === money);
+
+                *Single Line
+                console.log(6 === 6);
+
+            4. !== (Not Equal) That means the left side element will not to be equal with the right side.
+            Ex.
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary !== money);
+
+                *Single Line
+                console.log(6 !== 6);
+
+            5. >= (Greater then or Equal) That means the left side element will be greater or equal then the right side. 
+            Ex.
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary >= money);
+
+                *Single Line
+                console.log(6 >= 6);
+
+            6. <= (Less then or Equal) That means the left side element will be less then or equal then right side.
+            Ex.
+                const salary = 100000;
+                const money = 80000;
+                console.log(salary <= money);
+
+                *Single Line
+                console.log(6 <= 6);
+*/
+
